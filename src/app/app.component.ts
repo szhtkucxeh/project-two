ad57657cdb5f97f234cda841a3116c839a15bf29
szhtkucxeh <9998026@gmail.com>
2021-02-17 13:50:29 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
 [x: string]: any;
 
- 
- 
-    
-    
 
-coins=[{model:"aa",price:22},
-       {model:"bb",price:112}];
-} 
+}
